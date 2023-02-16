04f318fd340031af85e400f37a75485041c7d2dc
Mark Zemlianskyi <mark080501@gmail.com>
2023-02-16 14:18:53 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+import InfiniteScroll from 'infinite-scroll';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+var lightbox = new SimpleLightbox('.gallery a', {});
 const axios = require('axios').default;
 const refs = {
   container: document.querySelector('.gallery'),
   form: document.querySelector('form'),
+  imgRef: document.querySelector('.link'),
 };
 let query = null;
+let page = 1;
 // const BASE_URL = ;
-
-refs.form.addEventListener('submit', onSubmit);
+refs.container.addEventListener('click', onImgClick),
+  refs.form.addEventListener('submit', onSubmit);
+function onImgClick(e) {
+  // console.log(e.target);
+  // e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightbox.open();
+}
 function onSubmit(e) {
   e.preventDefault();
   query = refs.form.elements.searchQuery.value;
@@ -17,7 +31,7 @@ function onSubmit(e) {
 async function fetchImgs() {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api?key=33673211-c1a6432360cae6f7a6957d257&q=${query}&page=1&per_page=20&image_type=photo&orientation=horizontal&safesearch=false`
+      `https://pixabay.com/api?key=33673211-c1a6432360cae6f7a6957d257&q=${query}&page=${page}&per_page=10&image_type=photo&orientation=horizontal&safesearch=false`
     );
     return response;
   } catch (error) {
@@ -32,10 +46,12 @@ async function renderImg() {
 async function createCard(imgs) {
   console.log(imgs.data.hits);
   const markup = imgs.data.hits
-    .map(({ previewURL }) => {
+    .map(({ webformatURL, largeImageURL }) => {
       return `
-      <div class="photo-card">
-  <img src="${previewURL}" alt="" loading="lazy" />
+      <article class="photo-card post grid__item " >
+      <a href="${largeImageURL}" class="link">
+      <img class="previewImg" src="${webformatURL}" alt="" loading="lazy" />
+      </a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -50,9 +66,102 @@ async function createCard(imgs) {
       <b>Downloads</b>
     </p>
   </div>
-</div>
+</article>
     `;
     })
     .join('');
   refs.container.innerHTML = markup;
 }
+function increementPageValue() {
+  page += 1;
+}
+
+// let infScroll = new InfiniteScroll('.grid', {
+//   // Infinite Scroll options...
+//   path: fetchImgs(),
+//   append: '.grid__item',
+//   // outlayer: msnry,
+// });
+// let infScroll = new InfiniteScroll('.container', {
+//   // defaults listed
+
+//   path: increementPageValue(),
+//   // REQUIRED. Determines the URL for the next page
+//   // Set to selector string to use the href of the next page's link
+//   // path: '.pagination__next'
+//   // Or set with {{#}} in place of the page number in the url
+//   // path: '/blog/page/{{#}}'
+//   // or set with function
+//   // path: function() {
+//   //   return return '/articles/P' + ( ( this.loadCount + 1 ) * 10 );
+//   // }
+
+//   append: createCard(imgs),
+//   // REQUIRED for appending content
+//   // Appends selected elements from loaded page to the container
+
+//   checkLastPage: true,
+//   // Checks if page has path selector element
+//   // Set to string if path is not set as selector string:
+//   //   checkLastPage: '.pagination__next'
+
+//   prefill: false,
+//   // Loads and appends pages on intialization until scroll requirement is met.
+
+//   responseBody: 'text',
+//   // Sets the method used on the response.
+//   // Set to 'json' to load JSON.
+
+//   domParseResponse: true,
+//   // enables parsing response body into a DOM
+//   // disable to load flat text
+
+//   fetchOptions: undefined,
+//   // sets custom settings for the fetch() request
+//   // for setting headers, cors, or POST method
+//   // can be set to an object, or a function that returns an object
+
+//   outlayer: false,
+//   // Integrates Masonry, Isotope or Packery
+//   // Appended items will be added to the layout
+
+//   scrollThreshold: 400,
+//   // Sets the distance between the viewport to scroll area
+//   // for scrollThreshold event to be triggered.
+
+//   elementScroll: false,
+//   // Sets scroller to an element for overflow element scrolling
+
+//   loadOnScroll: true,
+//   // Loads next page when scroll crosses over scrollThreshold
+
+//   history: 'replace',
+//   // Changes the browser history and URL.
+//   // Set to 'push' to use history.pushState()
+//   //    to create new history entries for each page change.
+
+//   historyTitle: true,
+//   // Updates the window title. Requires history enabled.
+
+//   hideNav: undefined,
+//   // Hides navigation element
+
+//   status: undefined,
+//   // Displays status elements indicating state of page loading:
+//   // .infinite-scroll-request, .infinite-scroll-load, .infinite-scroll-error
+//   // status: '.page-load-status'
+
+//   button: undefined,
+//   // Enables a button to load pages on click
+//   // button: '.load-next-button'
+
+//   onInit: undefined,
+//   // called on initialization
+//   // useful for binding events on init
+//   // onInit: function() {
+//   //   this.on( 'append', function() {...})
+//   // }
+
+//   debug: false,
+//   // Logs events and state changes to the console.
+// });
